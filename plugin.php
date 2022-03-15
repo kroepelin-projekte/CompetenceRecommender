2653464b0562a512544ecc58f6d47623b76f7521
rob0403 <rob.falkenstein@rz.uni-freiburg.de>
2022-03-15 17:53:33 +0100

    updated plugin.php to il7

--- plugin.php
+++ plugin.php
@@ -8,8 +8,8 @@
 
 $id = "comprec";
 $version = "1.0.1";
-$ilias_min_version = "5.4.0";
-$ilias_max_version = "5.4.999";
+$ilias_min_version = "7.0";
+$ilias_max_version = "7.99";
 $responsible = "Leonie Feldbusch";
 $responsible_mail = "feldbusl@informatik.uni-freiburg.de";
 
