06e057639cb35f5094c9c3d6427d6d5bc99e7453
Keven Clausen <keven.clausen@kroeplin-projekte.de>
2025-04-03 14:04:28 +0200

    update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,8 +7,9 @@
  */
 
 $id = "comprec";
-$version = "1.1.3";
+$version = "2.0";
 $ilias_min_version = "8.0";
-$ilias_max_version = "9.99";
-$responsible = "fluxlabs ag";
-$responsible_mail = "support@fluxlabs.ch";
+$ilias_max_version = "8.99";
+
+$responsible = "Kröpelin Projekt GmbH";
+$responsible_mail = "info@kroepelin-projekte.de";
